3a987588d6617e6eabe18448c5c8ed25c6ef5603
noobow34 <skywind.hk34@gmail.com>
2025-10-22 07:42:51 +0900

    プロジェクト ファイルを追加します。

---
+++ FlareSync/DnsChange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlareSync
+{
+    internal class DnsChange
+    {
+        public int Order { get; set; }
+        public string Action { get; set; } = "";
+        public string FQDN { get; set; } = "";
+        public string Type { get; set; } = "";
+        public string Value { get; set; } = "";
+        public int TTL { get; set; }
+    }
+}
---
+++ FlareSync/Program.cs
@@ -0,0 +1,72 @@
+﻿using CloudFlareDns;
+using CloudFlareDns.Objects.Record;
+using FlareSync;
+using System.Text.Json;
+
+string inputJson = string.Empty;
+if (string.IsNullOrEmpty(args[0]))
+{
+    inputJson = Environment.GetEnvironmentVariable("FLARESYNC_INPUT") ?? "";
+}
+else
+{
+    inputJson = File.ReadAllText(args[0]);
+}
+
+List<DnsChange>? changes = JsonSerializer.Deserialize<List<DnsChange>>(inputJson)?.OrderBy(c => c.Order).ToList();
+if(changes == null || changes.Count == 0)
+{
+    Console.WriteLine("No DNS changes to process.");
+    return;
+}
+
+string xAuthKey = Environment.GetEnvironmentVariable("CLOUDFLARE_API_KEY") ?? "";
+string xAuthEmail = Environment.GetEnvironmentVariable("CLOUDFLARE_EMAIL") ?? "";
+string zoneIdentifier = Environment.GetEnvironmentVariable("CLOUDFLARE_ZONE_ID") ?? "";
+CloudFlareDnsClient cloudFlareDnsClient = new (xAuthKey, xAuthEmail, zoneIdentifier);
+
+foreach (DnsChange change in changes)
+{
+    Console.WriteLine($"Processing change: {JsonSerializer.Serialize(change)}");
+
+    var records = await cloudFlareDnsClient.Record.Get();
+    RecordType rt = change.Type.ToUpper() switch
+    {
+        "A" => RecordType.A,
+        "AAAA" => RecordType.AAAA,
+        "CNAME" => RecordType.CNAME,
+        "TXT" => RecordType.TXT,
+        "MX" => RecordType.MX,
+        "SRV" => RecordType.SRV,
+        "NS" => RecordType.NS,
+        "PTR" => RecordType.PTR,
+        "CAA" => RecordType.CAA,
+        _ => throw new Exception($"Unsupported record type: {change.Type}"),
+    };
+    var r = records.Where(r => r.Name == change.FQDN && r.Type == rt).FirstOrDefault();
+
+    switch (change.Action.ToLower())
+    {
+        case "upsert":
+            if(r != null)
+            {
+                r.Content = change.Value;
+                r.Ttl = change.TTL;
+                await cloudFlareDnsClient.Record.Update(r);
+            }
+            else
+            {
+                await cloudFlareDnsClient.Record.Create(change.FQDN, change.Value, false, rt, change.TTL);
+            }
+            break;
+        case "delete":
+            if(r != null)
+            {
+                await cloudFlareDnsClient.Record.Delete(r.Id);
+            }
+            break;
+        default:
+            Console.WriteLine($"Unknown action: {change.Action}");
+            break;
+    }
+}
